9bbf06722e93b8628ae4148b69732b292f5b9331
Shuai Zhang <szhang517@gmail.com>
2017-05-30 00:45:01 -0400

    update webpack config; add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <Router>
+        <div className="container-fluid">
+            <header>
+                The header..
+            </header>
+
+            <Route exact path="/" component={HomePage} />
+            <Route path="/about" component={AboutPage} />
+        </div>
+    </Router>,
+    document.getElementById('app')
+);
--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,6 @@ import expect from 'expect';
 
 describe('Our first test', () => {
   it('should pass', () => {
-    expect(true).toEqual(false);
+    expect(true).toEqual(true);
   });
 });
